1a4d19a37222abedc28985b2712b59abd3f51642
Kuldeep765 <kuldeep.jagani@techiebutler.com>
2024-03-12 05:26:14 -0700

    second commit

--- advance.js
+++ advance.js
@@ -18,7 +18,7 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 // e.push("smit")
 // console.log(e)
 
-// length()  // give array length 
+// length()  // give array length
 
 
 //  ---- Array method
@@ -59,13 +59,13 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 
 
 
-// const array1 = [1, 2, 3, 4,5];
+// const array1 = [1, 2, 3, 4, 5];
 // let sum = 0
 
 // const initialValue = 0;
 // const sumWithInitial = array1.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue,
+//     (accumulator, currentValue) => { console.log(currentValue, "current"); console.log(accumulator, "Accu"); sum = currentValue + accumulator; console.log(sum, "sum"); return sum; },
+//     initialValue,
 // );
 
 // for (let i = 0; i < array1.length; i++ ) {
@@ -81,14 +81,14 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 //   find min max value with spread operator
 // function findMinMax() {
 //     let Arr = [50, 60, 20, 10, 40];
- 
+
 //     let minValue = Math.min(...Arr);
 //     let maxValue = Math.max(...Arr);
-     
+
 //     console.log("element" + minValue);
 //     console.log("element" + maxValue);
 // }
- 
+
 // findMinMax()
 
 
@@ -114,7 +114,7 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 
 // 	//method
 // 	getFunction: function () {
-// 		return (`The name of the person is 
+// 		return (`The name of the person is
 // 		${person.first_name} ${person.last_name}`)
 // 	},
 
@@ -142,7 +142,7 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 
 
 
-// Defining class using 
+// Defining class using
 // class Vehicle {
 // 	constructor(name, maker, engine) {
 // 		this.name = name;
@@ -189,12 +189,12 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 // 	let lastname = lname;
 
 // 	let getDetails_noaccess = function () {
-// 		return (`First name is: ${firstname} Last 
+// 		return (`First name is: ${firstname} Last
 // 			name is: ${lastname}`);
 // 	}
 
 // 	this.getDetails_access = function () {
-// 		return (`First name is: ${firstname}, Last 
+// 		return (`First name is: ${firstname}, Last
 // 			name is: ${lastname}`);
 // 	}
 // }
@@ -217,7 +217,7 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 // }
 // class student extends person {
 // 	constructor(name, id) {
-// 		// super keyword for calling the above 
+// 		// super keyword for calling the above
 // 		// class constructor
 // 		super(name);
 // 		this.id = id;
@@ -237,23 +237,233 @@ const e = ['Ram', 'Karan', 'Vihsal'];
 //  Array with object
 
 
-let myArr = [
-    {
-     studenName: "jack",
-     studenClass: 6,
-     studenSection: "a"
-    },
-    {
-     studenName: "tom",
-     studenClass: 6,
-     studenSection: "a"
-    }
-];
+// let myArr = [
+//     {
+//         studenName: "jack",
+//         studenClass: 6,
+//         studenSection: "a"
+//     },
+//     {
+//         studenName: "tom1",
+//         studenClass: 6,
+//         studenSection: "a"
+//     },
+//     {
+//         studenName: "tom2",
+//         studenClass: 6,
+//         studenSection: "a"
+//     },
+//     {
+//         studenName: "tom3",
+//         studenClass: 6,
+//         studenSection: "a"
+//     }
 
-console.log(myArr[1]);
-console.log(myArr.constructor);
-myArr.map((value) => {
-	for(let property in value) {
-  	console.log(`${property}: ${value[property]}`);
-  }
+// ];
+
+// console.log(myArr[1]);
+// console.log(myArr.constructor);
+// myArr.map((value) => {
+//     for (let property in value) {
+//         console.log(`${property}: ${value[property]}`);
+//     }
+// });
+
+
+// let myObject = {
+//     studenName: "ram",
+//     studenClass: 6,
+//     studenSection: "a"
+// };
+
+// myArr.push(myObject)
+
+// myArr.splice(1, 1)
+// console.log(myArr)
+
+
+// var removeByAttr = function (arr, attr, value) {
+//     var i = arr.length;
+//     while (i--) { //3 2 1 arr[2]
+//         // console.log(arguments)
+//         if (arr[i] && arr[i].hasOwnProperty(attr) && (arguments.length > 2 && arr[i][attr] === value)) {
+
+//             // arr.splice(i, 1);
+
+//         }
+//     }
+//     return arr;
+// }
+
+// var arr = [{ id: 1, name: 'serdar' }, { id: 2, name: 'alfalfa' }, { id: 3, name: 'joe' }];
+// // console.log(arr)
+// // console.log(removeByAttr(arr, 'id', 1))
+
+// removeByAttr(arr, 'name', 'joe');
+
+
+
+// sort Array
+
+// const arr = [4, 56, 5, 3, 34, 37, 89, 57, 98];
+// console.log(arr)
+
+// const sortWithReduce = arr => {
+//     return arr.reduce((acc, val) => {
+//         // console.log(acc)
+//         // console.log(val)
+//         let ind = 0;
+
+//         while (ind < arr.length && val < arr[ind]) {
+//             ind++;
+//             console.log(ind)
+//             // console.log(ind)
+//         }
+//         acc.splice(ind, 0, val);
+//         return acc;
+//     }, []);
+// };
+// console.log(sortWithReduce(arr));
+
+
+// const points = [40, 100, 1, 5, 25, 10];
+
+// points.sort(function (a, b) {
+//     console.log(a, "A value")
+//     console.log(b, "B value");
+
+//     return a - b
+// });
+
+
+
+// Array with loop
+
+var fruits = ["apple", "banana", "cherry", "date"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+
+// fruits.forEach(function (fruit) {
+//     console.log(fruit);
+// });
+
+// fruits.forEach((fruits) => {
+//     console.log(fruits)
+// })
+
+// var people = [
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 30 },
+//     { name: "Charlie", age: 35 }
+// ];
+
+// for (var person of people) {
+//     console.log(person)
+//     for (var key in person) {
+//         // console.log(key)
+//         console.log(key + ": " + person[key]);
+//     }
+// }
+
+
+
+// Promisee and try_catch
+
+// function printMe() {
+//     console.log('print me');
+// }
+
+// setTimeout(printMe, 2000);
+
+
+// function printMe() {
+//     console.log('print me');
+// }
+
+// function test() {
+//     console.log('test');
+// }
+
+// setTimeout(printMe, 2000);
+// test();
+
+let promise = new Promise(function (resolve, reject) {
+    const x = "ram";
+    const y = "ram"
+    if (x === y) {
+        resolve();
+    } else {
+        reject();
+    }
 });
+
+promise.
+    then(function () {
+        console.log('Success, You are a GEEK');
+    }).
+    then(function () {
+        console.log('middle value, You are a GEEK');
+    }).
+    catch(function () {
+        console.log('Some error has occurred');
+    });
+
+
+
+// call function
+const person = {
+    fullName: function (city, country) {
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+}
+
+const person1 = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+const person2 = {
+    firstName: "Mary",
+    lastName: "Doe"
+}
+
+const a = person.fullName.call(person1, "Oslo", "Norway");
+// console.log(a)
+
+
+// apply() function same as call but you give argument as array
+
+
+
+
+
+const user = {
+    username: 'AzureDiamond',
+    password: 'hunter2'
+};
+
+// Freeze the object
+const newUser = Object.seal(user);
+
+// newUser.password = '*******';
+// newUser.active = true;
+
+// console.log(newUser);
+
+
+// const user = {
+// 	username: 'AzureDiamond',
+// 	password: 'hunter2'
+// };
+
+// // Seal the object
+// const newUser = Object.seal(user);
+// console.log(Object.seal)
+
+// newUser.password = '*******';
+// newUser.active = true;
+
+// console.log(newUser);
